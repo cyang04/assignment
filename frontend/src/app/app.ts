6c66af858a8f456d676546bf1ce01cb2c3cf478e
g <wickedzfrost@gmail.com>
2026-09-11 17:17:20 +0800

    Complete angular flashcard crud

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,32 +1,109 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, signal} from '@angular/core';
+import { FormsModule} from '@angular/forms';
 import { Flashcard } from './model/flashcard';
 import { FlashcardService } from './service/flashcard.service';
 
 @Component({
   selector: 'app-root',
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
 
-  flashcards: Flashcard[] = [];
+  flashcards = signal<Flashcard[]>([]);
+  newFlashcard: Partial<Flashcard> = {
+    question: '',
+    answer: '',
+    category: '',
+  }
+
+  editingId: number | null = null;
+  editFlashcard: Partial<Flashcard> = {
+    question: '',
+    answer: '',
+    category: ''
+  };
 
   constructor(private flashcardService: FlashcardService) {}
 
   ngOnInit(): void {
+    this.loadFlashcards();
+  }
+
+  loadFlashcards(): void {
     this.flashcardService.getFlashcards().subscribe({
       next: (data) => {
-        console.log('DATA:', data);
-        console.log('DATA LENGTH:', data.length);
-
-        this.flashcards = data;
-
-        console.log('SAME REFERENCE:', this.flashcards === data);
-        console.log('FLASHCARDS:', this.flashcards);
-        console.log('FLASHCARDS LENGTH:', this.flashcards.length);
+        this.flashcards.set(data);
       },
       error: (error) => {
         console.error('Failed to load flashcards', error);
+      }
+    });
+  }
+
+  createFlashcard(): void {
+    this.flashcardService.createFlashcard(this.newFlashcard).subscribe({
+      next: (created) => {
+        this.flashcards.update(list => [...list, created]);
+
+        this.newFlashcard = {
+          question: '',
+          answer: '',
+          category: '',
+        };
+      },
+      error: (error) => {
+        console.error(`Failed to create flashcard`, error);
+      }
+    });
+  }
+
+  startEdit(flashcard: Flashcard): void {
+    this.editingId = flashcard.id;
+
+    this.editFlashcard = {
+      question: flashcard.question,
+      answer: flashcard.answer,
+      category: flashcard.category
+    };
+  }
+
+  saveEdit(): void {
+    if (this.editingId === null) {
+      return;
+    }
+
+    this.flashcardService
+      .updateFlashcard(this.editingId, this.editFlashcard)
+      .subscribe({
+        next: (updated) => {
+          this.flashcards.update(list =>
+            list.map(f =>
+              f.id === updated.id ? updated : f
+            )
+          );
+
+          this.editingId = null;
+          this.editFlashcard = {
+            question: '',
+            answer: '',
+            category: ''
+          };
+        },
+        error: (error) => {
+          console.error('Failed to update flashcard', error);
+        }
+      });
+  }
+
+  deleteFlashcard(id: number): void {
+    this.flashcardService.deleteFlashcard(id).subscribe({
+      next: () => {
+        this.flashcards.update(list => list.filter(f => f.id !== id));
+      },
+      error: (error) => {
+        console.error('delete error:', error);
       }
     });
   }
--- frontend/src/app/service/flashcard.service.ts
+++ frontend/src/app/service/flashcard.service.ts
@@ -15,4 +15,16 @@ export class FlashcardService {
   getFlashcards(): Observable<Flashcard[]> {
     return this.http.get<Flashcard[]>(this.apiUrl);
   }
+
+  createFlashcard(flashcard: Partial<Flashcard>): Observable<Flashcard> {
+    return this.http.post<Flashcard>(this.apiUrl, flashcard);
+  }
+
+  updateFlashcard(id: number, flashcard: Partial<Flashcard>): Observable<Flashcard> {
+    return this.http.put<Flashcard>(`${this.apiUrl}/${id}`, flashcard);
+  }
+
+  deleteFlashcard(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
